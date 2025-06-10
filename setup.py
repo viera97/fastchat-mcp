1d7b21fbd9fcddd8635c1b38e285e1d4ca84e28b
rb58853 <rb58853@gmail.com>
2025-06-10 17:26:53 -0400

    Add setup to publish

--- setup.py
+++ setup.py
@@ -1,19 +1,31 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+import os
+
+try:
+    HERE = Path(__file__).parent.resolve()
+    README = HERE / "readme.md"
+
+    with open(README, encoding="utf-8") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = "readme.md no encontrado. Por favor, revise la documentación."
+except Exception as e:
+    long_description = f"Error leyendo readme.md: {str(e)}"
 
 setup(
     name="mcp-client",
     version="1.0.0",
     packages=find_packages(),
-    author="Tu Nombre",
+    author="Raúl Beltrán Gómez",
     author_email="rb58853@gmail.com",
-    description="Descripción breve de mi proyecto",
-    long_description=open("readme.md").read(),
+    description="Python client, based on `fastmcp`, for connecting to MCP servers through multiple protocols, specifically designed to work with integrated language models.",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rb58853/python-mcp-client",
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.11",
