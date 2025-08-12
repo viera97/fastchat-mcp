39e4bd5bd7140ed834ecc553684dd723467949bb
rb58853 <rb58853@gmail.com>
2025-08-12 10:04:49 -0400

    update default model to `gpt-5-nano`

--- src/fastchat/config/llm_config.py
+++ src/fastchat/config/llm_config.py
@@ -17,10 +17,19 @@ class ConfigGPT:
             "input": 0.15 / 1000000,
             "output": 0.60 / 1000000,
         },
+        "gpt-5-mini": {
+            "input": 0.25 / 1000000,
+            "output": 2.00 / 1000000,
+        },
+        "gpt-5-nano": {
+            "input": 0.05 / 1000000,
+            "output": 0.40 / 1000000,
+        },
     }
     """Price by one tokens from each model"""
 
-    DEFAULT_MODEL_NAME = "gpt-4o-mini"
+    # DEFAULT_MODEL_NAME = "gpt-4o-mini"
+    DEFAULT_MODEL_NAME = "gpt-5-nano"
     """GPT model that will be used"""
 
     OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
